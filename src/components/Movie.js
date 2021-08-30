0f792921f3201586aeddbd099240b0de1ca7bf3c
DUY HUYNH BAO NGUYEN <20746598@students.latrobe.edu.au>
2021-08-30 22:02:41 +1000

    add useMovieFetch hook

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,26 @@
 import React from 'react'
+import {useParams} from 'react-router-dom'
+//Config
+import {IMAGE_BASE_URL, POSTER_SIZE} from '../config'
 
-const Movie = () => <div>Movie</div>
+//components
+import Grid from './Grid'
+import Spinner from './Spinner'
+
+//hook
+import { useMovieFetch } from '../hooks/useMovieFetch'
+//image
+import NoImage from '../images/no_image.jpg'
+const Movie = () =>  {
+    const {movieId} = useParams();
+    const {state:movie, loading, error} = useMovieFetch(movieId)
+
+    console.log(movie);
+    return (
+        <React.Fragment>
+
+        </React.Fragment>
+    )
+}
 
 export default Movie;
