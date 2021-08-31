a70d287adf338b62b2f281495d943e836b41c5a9
DUY HUYNH BAO NGUYEN <20746598@students.latrobe.edu.au>
2021-08-31 15:43:32 +1000

    add actor and styles

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -9,6 +9,7 @@ import Spinner from './Spinner'
 import BreadCrumb from './BreadCrumb'
 import MovieInfo from './MovieInfo'
 import MovieInfoBar from './MovieInfoBar'
+import Actor from './Actor'
 //hook
 import { useMovieFetch } from '../hooks/useMovieFetch'
 //image
@@ -28,6 +29,20 @@ const Movie = () =>  {
                 budget = {movie.budget}
                 revenue = {movie.revenue}
             />
+            <Grid header='Actors'>
+                {movie.actors.map(actor=>(
+                    <Actor 
+                        key = {actor.credit_id}
+                        name ={actor.name}
+                        character ={actor.character}
+                        imageURL ={
+                            actor.profile_path
+                            ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+                            : NoImage
+                        }
+                    />    
+                ))}
+            </Grid>
         </React.Fragment>
     )
 }
