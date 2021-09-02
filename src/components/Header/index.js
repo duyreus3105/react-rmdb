f7642dca560f81f9d782807ac38c94c854be5cd9
DUY HUYNH BAO NGUYEN <20746598@students.latrobe.edu.au>
2021-09-02 20:56:57 +1000

    add rating

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import RMDBLogo from '../../images/react-movie-logo.svg';
 import TMDBLogo from '../../images/tmdb_logo.svg';
 import { Wrapper, Content, LogoImg, TMDBLogoImg } from './Header.styles';
 import {Link} from 'react-router-dom';
 
-const Header = () =>(
-    <Wrapper>
-        <Content>
-            <Link to="/">
-                <LogoImg src={RMDBLogo} alt='rmdb-logo'/>
-            </Link>
-            <TMDBLogoImg src ={TMDBLogo} alt='tmdb-logo'/>
-        </Content>
-    </Wrapper>
-) 
+//Context
+import {Context} from '../../context';
+
+const Header = () =>{    
+    const [user] = useContext(Context);
+    console.log(user)
+    return(
+        <Wrapper>
+            <Content>
+                <Link to="/">
+                    <LogoImg src={RMDBLogo} alt='rmdb-logo'/>
+                </Link>
+                {user? (
+                    <span className='loggedin'>Logged in as: {user.username}</span>
+                ):(
+                    <Link to='Login'>
+                        <span className ='login'>Log in</span>
+                    </Link>
+                )
+                }
+                <TMDBLogoImg src ={TMDBLogo} alt='tmdb-logo'/>
+            </Content>
+        </Wrapper>
+    ) 
+}
 
 export default Header;
